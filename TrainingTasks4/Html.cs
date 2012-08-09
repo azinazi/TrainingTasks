d74b996b874a1f20cf882b8bd4f34297c69bc499
Azadeh Sadeghieh <azadeh_sad@yahoo.com>
2012-08-09 15:23:01 +1000

    code compiles now

---
+++ TrainingTasks4/Html.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrainingTasks4
+{
+    class Html
+    {
+
+        public static Html Tag(string tag)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public Html Attr(string p, string p_2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Html Modify(Action<Html> action)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Html AddClass(string p)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
